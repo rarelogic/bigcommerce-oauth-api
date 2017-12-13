d9f5de1e6205e63e7a04eb2a32880fd2bf879a10
Nick Sweeting <nsweeting@gmail.com>
2017-12-13 14:42:26 -0500

    Add categories_count method

--- lib/bigcommerce-oauth-api/client.rb
+++ lib/bigcommerce-oauth-api/client.rb
@@ -24,7 +24,7 @@ module BigcommerceOAuthAPI
     with_api_methods :blog_post => { api_module: :post, scope: :self, methods: [:all, :select, :create, :update, :delete], prefix_paths: 'blog', prefix_methods: 'blog'},
                      :blog_tag => { api_module: :tag, scope: :self, methods: [:all], prefix_paths: 'blog', prefix_methods: 'blog'},
                      :brand => { api_module: :brand, scope: :self, methods: [:all, :select, :create, :update, :delete, :count]},
-                     :category => { api_module: :category, scope: :self, methods: [:all, :select, :create, :update, :delete]},
+                     :category => { api_module: :category, scope: :self, methods: [:all, :select, :create, :update, :delete, :count]},
                      :customer => { api_module: :customer, scope: :self, methods: [:all, :select, :create, :update, :delete, :count]},
                      :customer_address => { api_module: :address, scope: :customer, methods: [:all, :select, :create, :update, :delete]},
                      :customer_group => { api_module: :customer_group, scope: :self, methods: [:all, :select, :create, :update, :delete]},
@@ -115,6 +115,10 @@ module BigcommerceOAuthAPI
     # @!method categories(options = {})
     # @param [Hash] options the filters for the categories
     # gets a list of categories
+
+    # @!method categories_count(options = {})
+    # @param [Hash] options the filters for the categories
+    # returns the number of categories
 
     # @!method category(id, options = {})
     # @param [Integer] id the identifier for the category
